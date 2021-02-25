956cc7cd8eb98552d70dd696e9b7f097edec931f
KunRalf <rshumaher1996@gmail.com>
2021-02-25 17:22:53 +0300

    feat: compete task

--- src/index.js
+++ src/index.js
@@ -2,5 +2,18 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-  return [];
+  arrMatrix = matrix;
+  if (!arrMatrix){
+    return [];
+  }
+  let arr = [];
+  for (let i =0; i<arrMatrix.length; i++){
+    if (i % 2 !== 0){
+      arr = arr.concat((arrMatrix[i]).reverse());
+    }
+    else {
+      arr = arr.concat(arrMatrix[i]);
+    }
+  }
+  return arr;
 }
